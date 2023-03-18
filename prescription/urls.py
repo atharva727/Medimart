e59aedc229b3a3c3fe91a674a607fb872d010d98
Atharva Wadpelli <atharvawadpelli727@gmail.com>
2023-03-18 14:57:32 +0530

    Developed Index Page

--- prescription/urls.py
+++ prescription/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from prescription.views import *
 
 urlpatterns = [    
-    path('', Index.as_view()),
-    path('login/', Login.as_view()),
-    path('prescription/', Prescription.as_view()),
-    path('result/', Result.as_view()),
+    path('', Index.as_view(),name='home'),
+    path('login/', Login.as_view(),name='login'),
+    path('prescription/', Prescription.as_view(),name='prescription'),
+    path('result/', Result.as_view(), name='result'),
 ]
